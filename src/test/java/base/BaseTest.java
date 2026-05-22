69789de01527cb98763f2772d35b65c942851096
Slava <v.tkalenko.work@gmail.com>
2026-05-22 09:52:30 +0300

    Added base framework structure

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,10 +1,13 @@
 package base;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 
 public class BaseTest {
     @BeforeEach
     public void setup(){
-
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 10000;
     }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,4 +1,10 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+
 public class LoginTests {
+    @Test
+    public void firstTest(){
+        System.out.println("Test started");
+    }
 }
